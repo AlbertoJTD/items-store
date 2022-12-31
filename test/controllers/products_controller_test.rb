0c9d8aab16a28b34cd6bdb82b24d6633aefc6d8f
AlbertoJTD <albertojtd@outlook.com>
2022-12-30 20:58:53 -0600

    Testing products controller

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,7 +2,15 @@ require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get products_index_url
+    get products_path
+
+    assert_response :success
+    assert_select '.product', 2
+  end
+
+  test 'should show a product' do
+    get product_path(products(:ps4))
+
     assert_response :success
   end
 end
