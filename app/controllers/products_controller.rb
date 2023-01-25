598aaa6fff22589d4bc76f188b9d5409560357d2
AlbertoJTD <albertojtd@outlook.com>
2023-01-24 21:42:33 -0600

    Delete method added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,6 +35,13 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to products_path, notice: 'Product deleted'
+  end
+
   private
 
   def product_params
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -75,4 +75,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :unprocessable_entity
   end
+
+  test 'should delete product' do
+    assert_difference('Product.count', -1) do
+      delete product_path(products(:ps4))
+    end
+
+    assert_redirected_to products_path
+    assert_equal flash[:notice], 'Product deleted'
+  end
 end
