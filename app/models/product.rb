e547f34d9a2880770ef4a55389af24bc462bad33
AlbertoJTD <albertojtd@outlook.com>
2023-02-05 14:28:23 -0600

    Add category_id to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,15 @@
 #  title       :string           not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  category_id :bigint           not null
+#
+# Indexes
+#
+#  index_products_on_category_id  (category_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
 #
 class Product < ApplicationRecord
   has_one_attached :photo
