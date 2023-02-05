e5b6c93e13f0bb9c4e648d25d206030804eebbe2
AlbertoJTD <albertojtd@outlook.com>
2023-02-04 20:42:30 -0600

    Show image if exist

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,6 +45,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:description, :price, :title)
+    params.require(:product).permit(:description, :price, :title, :photo)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -10,6 +10,8 @@
 #  updated_at  :datetime         not null
 #
 class Product < ApplicationRecord
+  has_one_attached :photo
+
   validates :title, presence: :true
   validates :description, presence: :true
   validates :price, presence: :true
