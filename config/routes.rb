41cda8ec3c5e8f92311f1b71928598e29eca4052
AlbertoJTD <albertojtd@outlook.com>
2023-03-15 21:53:34 -0600

    destroy session & added i18n dates for spanish

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@
 
 Rails.application.routes.draw do
   root "products#index"
+  
+  namespace :authentication, path: '', as: '' do
+    resources :users, only: [:new, :create], path: '/register', path_names: { new: '/' }
+    resources :sessions, only: [:new, :create, :destroy], path: '/login', path_names: { new: '/' }
+  end
+
   resources :products
   resources :categories, except: :show
-
-  namespace :authentication, path: '', as: '' do
-    resources :users, only: [:new, :create]
-    resources :sessions, only: [:new, :create]
-  end
 end
