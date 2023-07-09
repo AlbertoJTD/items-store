84ec3fd8c62e5070d2bc2178ff7c18c259ff184d
AlbertoJTD <albertojtd@outlook.com>
2023-07-09 16:16:03 -0600

    Update routes and gemfile.lock

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root "products#index"
-  
+  root 'products#index'
+
   namespace :authentication, path: '', as: '' do
-    resources :users, only: [:new, :create], path: '/register', path_names: { new: '/' }
-    resources :sessions, only: [:new, :create, :destroy], path: '/login', path_names: { new: '/' }
+    resources :users, only: %i[new create], path: '/register', path_names: { new: '/' }
+    resources :sessions, only: %i[new create destroy], path: '/login', path_names: { new: '/' }
   end
 
   resources :products
