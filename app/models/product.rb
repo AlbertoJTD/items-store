e691c9544abafca21e49a6e5c8015accf045b983
AlbertoJTD <albertojtd@outlook.com>
2023-09-06 13:02:08 -0600

    Add Favoritable concern to handle favorites methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -23,6 +23,8 @@
 #
 class Product < ApplicationRecord
   include PgSearch::Model
+  include Favoritable
+
   pg_search_scope :search_full_text, against: {
     title: 'A',
     description: 'B'
@@ -40,23 +42,10 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true
 
-  has_many :favorites, dependent: :destroy
   belongs_to :category
   belongs_to :user, default: -> { Current.user }
 
   def owner?
     user_id == Current.user&.id
-  end
-
-  def favorite!
-    favorites.create(user: Current.user)
-  end
-
-  def unfavorite!
-    favorite.destroy
-  end
-
-  def favorite
-    favorites.find_by(user: Current.user)
   end
 end
