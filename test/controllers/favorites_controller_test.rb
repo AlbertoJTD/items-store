3e3891f1c14c0c401b38891ab96a5298aa5d05b4
AlbertoJTD <albertojtd@outlook.com>
2023-09-06 12:37:19 -0600

    Add test to remove favorite

--- test/controllers/favorites_controller_test.rb
+++ test/controllers/favorites_controller_test.rb
@@ -3,14 +3,23 @@ require 'test_helper'
 class FavoritesControllerTest < ActionDispatch::IntegrationTest
   setup do
     login
-    @product = products(:ps4)
+    @ps4 = products(:ps4)
+    @nvidia = products(:nvidia)
   end
 
   test 'should create favorite' do
     assert_difference('Favorite.count', 1) do
-      post favorites_url(product_id: @product.id)
+      post favorites_url(product_id: @ps4.id)
     end
 
-    assert_redirected_to product_path(@product)
+    assert_redirected_to product_path(@ps4)
+  end
+
+  test 'should remove favorite' do
+    assert_difference('Favorite.count', -1) do
+      delete favorite_url(@nvidia.id)
+    end
+
+    assert_redirected_to product_path(@nvidia)
   end
 end
